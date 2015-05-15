acc71a5e59a8356680b3dfd005fe3d3f726d7ce5
SalahELGO <salah-eddine.el-gouacem@etu.univ-nantes.fr>
2015-05-15 02:51:29 +0200

    pioupiou

---
+++ detect.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+a = 18
+b = 25
+
+age = 16
+
+if age > a and age < b:
+	print("""vous etes dans l\'intervalle""")
+elif age < 18:
+	print("vous etes trop jeune, cassez vous !")
+else:
+	print('mais vous etes trop vieux')
